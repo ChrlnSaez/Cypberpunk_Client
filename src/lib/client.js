471e21e5ec7d59d3794a614573aa651507a4dfb5
ChrlnSaez <charlenesaez03@gmail.com>
2022-09-03 14:07:26 +0800

    Change base url

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://cyberpunk-api.vercel.app/api',
 });
 
 client.interceptors.request.use(async (config) => {
